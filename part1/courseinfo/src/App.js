611265b31b8fe8f4c9a735e37ae128746355d933
Ezechimere Wise <ikehezechi@gmail.com>
2022-10-11 19:20:01 +0100

    courseinfo completed

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,56 @@
+
+const Header = (props) => <h1>{props.course}</h1>
+
+const Part = ({name, exercises}) => <p>{name} {exercises}</p>
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises}/>
+      <Part name={parts[1].name} exercises={parts[1].exercises}/>
+      <Part name={parts[2].name} exercises={parts[2].exercises}/>
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  const [part1, part2, part3] = parts
+  const totalExercises = part1.exercises + part2.exercises + part3.exercises
+
+  return (
+    <div>
+      <p>Number of exercises {totalExercises}</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div className="App">
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  );
+}
+
+export default App;
